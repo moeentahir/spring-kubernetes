961bd47c262dd381917a4f28b1ac377d3a7d44a7
Moeen Tahir <moeentahir@hotmail.com>
2021-03-12 18:31:14 +0000

    Initial commit

---
+++ src/main/java/org/ss/demo/SpringKubernetesApplication.java
@@ -0,0 +1,13 @@
+package org.ss.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKubernetesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKubernetesApplication.class, args);
+	}
+
+}
